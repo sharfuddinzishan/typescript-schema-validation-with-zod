61199f688ecd7e036e2b3a8bc21cb27e5f622850
sharfuddin.zishan <sharfuddin.ahamed91@gmail.com>
2023-11-26 20:31:49 +0600

    Partial, Required, Passthrough, Strict

---
+++ src/docs/passthrough-strict.ts
@@ -0,0 +1,55 @@
+import { z } from 'zod'
+
+/*
+Default:strip out unrecognized keys during parsing.
+Passthrough: to pass through unknown keys
+Strict: If there are any unknown keys in the input, Zod will throw an error (ZodError for parse).
+*/
+
+// Default Parsing
+const getInfo = z
+  .object({
+    name: z.string(),
+    age: z.number(),
+    salary: z.number().optional(),
+  })
+  .parse({
+    name: 'Zishan',
+    age: 23,
+    dept: 'CSE',
+  })
+
+console.log(getInfo) // { name: 'Zishan', age: 23 }
+
+///////////////////////////////////////////////////////////
+const getInfoPass = z
+  .object({
+    name: z.string(),
+    age: z.number(),
+    salary: z.number().optional(),
+  })
+  .passthrough()
+  .parse({
+    name: 'Zishan',
+    age: 23,
+    dept: 'CSE',
+  })
+
+console.log(getInfoPass) // { name: 'Zishan', age: 23, dept: 'CSE' }
+
+///////////////////////////////////////////////////////////
+
+const getInfoStrict = z
+  .object({
+    name: z.string(),
+    age: z.number(),
+    salary: z.number().optional(),
+  })
+  .strict()
+  .parse({
+    name: 'Zishan',
+    age: 23,
+    dept: 'CSE',
+  })
+
+console.log(getInfoStrict) // Unrecognized key(s) in object: 'dept'
---
+++ src/docs/required-partial.ts
@@ -0,0 +1,27 @@
+import { z } from 'zod'
+
+/*
+require: makes all properties required, but you can specify which property be required
+partial: makes all field optional
+*/
+
+// All Fields set to optional()
+const infoSchema = z
+  .object({
+    name: z.string(),
+    email: z.string().email(),
+    age: z.number(),
+    salary: z.number(),
+    role: z.enum(['User', 'Admin', 'Editor']),
+    password: z.string(),
+    confirmPassword: z.string(),
+  })
+  .partial()
+
+//   name, email, password, confirmPassword is now required
+const authSchema = infoSchema.required({
+  name: true,
+  email: true,
+  password: true,
+  confirmPassword: true,
+})
